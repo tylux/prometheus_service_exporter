063baa6a1d67e199045ace79941b0231ebe1b0ab
Tyler Dockstader <tyler.dockstader@NUVI0161.local>
2017-07-17 15:36:24 -0600

    add ability to take multiple comma-separted services

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Prometheus Service Exporter
 
-Prometheus Service Exporter is meant to be a way to monitor if a systemd service is active or not.
+Prometheus Service Exporter is meant to be a way to monitor if a Systemd service is active or not.
 
 Useful for times when there isn't a full exporter for the service and you need to monitor if it is up or not.
 
@@ -8,13 +8,16 @@ Useful for times when there isn't a full exporter for the service and you need t
 
   - Download latest AMD64 compiled binary from realeases tab and place under /usr/local/bin/prometheus_service_exporter OR if you have a local Golang environment setup run ```Make linux``` to compile it yourself
   - Install the prometheus-service-exporter.service file under /etc/systemd/system/prometheus-service-exporter.service
-  - Edit prometheus-service-exporter.service file to monitor your service "defaults to sshd.service"
+  - Edit prometheus-service-exporter.service file to monitor your service, if you want to monitor multiple, use a comma-seperated list "defaults to sshd.service"
   - Run ```systemctl daemon-reload``` and ```systemctl start prometheus-service-exporter.service```
 
-# Test
+# Test and Example output
 Run ```curl localhost:8080/metrics```
 ```sh
-# HELP service_up Check if the Service is up.
+# HELP service_up Is the service active
 # TYPE service_up gauge
-service_up 1
+service_up{service="apache2"} 0
+service_up{service="kibana"} 1
+service_up{service="nginx"} 1
+service_up{service="sshd"} 1
 ```
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os/exec"
+	"strings"
 
 	"github.com/kelseyhightower/envconfig"
 	"github.com/prometheus/client_golang/prometheus"
@@ -19,25 +21,55 @@ type Specification struct {
 }
 
 var (
-	service = flag.String("s", "", "Name of the service you wish to monitor")
+	service = flag.String("s", "", "A comma separated list of services you want to monitor")
+	pm      = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "service_up",
+		Help: "Is the service active",
+	}, []string{"service"})
 )
+
+func init() {
+	prometheus.MustRegister(pm)
+}
+
+func serviceCheck(s string) float64 {
+	//Command to check if systemd service is active
+	var up float64
+	cmdName := "/bin/systemctl"
+	cmdArgs := []string{"is-active", s}
+	cmdOut, _ := exec.Command(cmdName, cmdArgs...).Output()
+	isActive := strings.TrimSpace(string(cmdOut))
+
+	if isActive == "active" {
+		up = 1
+	} else {
+		up = 0
+	}
+	return up
+}
 
 func main() {
 	flag.Parse()
 	var s Specification
+
 	err := envconfig.Process("", &s)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+
 	log.Println("Starting Exporter...")
+	serviceSlice := strings.Split(*service, ",")
 
 	if *service == "" {
 		fmt.Println("You need to define a service to monitor.")
-	} else {
-		serviceString := string(*service)
-		exporter := NewExporter(serviceString)
-		prometheus.MustRegister(exporter)
 	}
+
+	go func() {
+		for i := range serviceSlice {
+			x := serviceCheck(serviceSlice[i])
+			pm.With(prometheus.Labels{"service": serviceSlice[i]}).Set(x)
+		}
+	}()
 
 	log.Printf("Starting Server: %s\n", s.ListenAddress)
 	log.Printf("Metrics Path: %s\n", s.MetricsPath)
@@ -61,4 +93,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 }
